59c1f0967bda07baf99fd053e991e84fa3d748ee
MIN <com.min.workspace@gmail.com>
2024-10-19 16:12:59 +0800

    add ipfs

--- src/controller/ai/openai.controller.ts
+++ src/controller/ai/openai.controller.ts
@@ -1,75 +1,73 @@
-import { getImages, saveImage } from '@/services/prisma/image';
-import { generateImage, openaiCreateVariation } from '@/services/openai';
-import { NextFunction, Request, Response } from 'express';
-import { uploadImageFromUrl } from '@/services/aws/upload';
-
+import { getImages, saveImage } from "@/services/prisma/image"
+import { generateImage, openaiCreateVariation } from "@/services/openai"
+import { NextFunction, Request, Response } from "express"
+import { pinFile } from "@/services/pinata"
 
 class OpenAIController {
-    public async generateTextToImage(req: Request, res: Response, next: NextFunction) {
+  public async generateTextToImage(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const { prompt, size, quality } = req.body
+      const response = await generateImage(prompt, size, quality)
+      if (!response.url) throw new Error("no url generated")
 
-        try {
+      const storedImg = await pinFile(response.url, req.user.id)
+      if (!storedImg) throw new Error("Failed to pin image file")
 
-            const { prompt, size, quality } = req.body;
-            const response = await generateImage(prompt, size, quality)
-            if (!response.url) throw new Error('no url generated')
+      await saveImage(req.user.id, {
+        url: storedImg,
+        type: "generate",
+        prompt: prompt,
+        model: "dall-e-3",
+        size: size,
+        quality: quality,
+      })
 
-            const storedImg = await uploadImageFromUrl(response.url, req.user.id)
-
-            await saveImage(req.user.id, {
-                url: storedImg.Location,
-                type: "generate",
-                prompt: prompt,
-                model: 'dall-e-3',
-                size: size,
-                quality: quality,
-            })
-
-            res.status(200).json(response);
-        } catch (error) {
-            next(error);
-        }
+      res.status(200).json(response)
+    } catch (error) {
+      next(error)
     }
+  }
 
-    public async generateVariation(req: Request, res: Response, next: NextFunction) {
+  public async generateVariation(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const { size } = req.body
 
-        try {
+      const imgUrl = await openaiCreateVariation(req.file, size)
 
-            const { size } = req.body;
+      const storedImg = await pinFile(imgUrl, req.user.id)
+      if (!storedImg) throw new Error("Failed to pin image file")
 
-            const imgUrl = await openaiCreateVariation(req.file, size)
+      await saveImage(req.user.id, {
+        url: storedImg,
+        type: "variation",
+        prompt: "",
+        model: "dall-e-3",
+        size: size,
+        quality: "standard",
+      })
 
-            const storedImg = await uploadImageFromUrl(imgUrl, req.user.id)
-
-            await saveImage(req.user.id, {
-                url: storedImg.Location,
-                type: "variation",
-                prompt: "",
-                model: 'dall-e-3',
-                size: size,
-                quality: 'standard',
-            })
-
-            res.status(200).json(storedImg.Location);
-        } catch (error) {
-
-            next(error);
-        }
+      res.status(200).json(storedImg)
+    } catch (error) {
+      next(error)
     }
+  }
 
-
-    public async getAllImages(req: Request, res: Response, next: NextFunction) {
-
-        try {
-
-            const images = await getImages()
-            res.status(200).json(images);
-        } catch (error) {
-
-            next(error);
-        }
+  public async getAllImages(req: Request, res: Response, next: NextFunction) {
+    try {
+      const images = await getImages()
+      res.status(200).json(images)
+    } catch (error) {
+      next(error)
     }
-
-
+  }
 }
 
-export default OpenAIController;
+export default OpenAIController
