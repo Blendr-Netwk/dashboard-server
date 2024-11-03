87218db51a3dd45bc37b93376c508a8b6a2d1a31
pp@Blendr <dev.sblendr@gmail.com>
2024-11-03 12:29:33 +0000

    update rent duration with shorter time for testing purposes

--- src/constant/reward.ts
+++ src/constant/reward.ts
@@ -1,37 +1,39 @@
-import { DAY } from "./time";
+import { DAY,MINTUES } from "./time";
 
-export const REWARD_DURATION = 1 * DAY
+const DURATION_UNIT = MINTUES
+
+export const REWARD_DURATION = 1 * DURATION_UNIT
 
 export const REWARD_PENALTY = 0.75
 
 export const REWARD_LENDS = [
   {
     id: "1",
-    lendPeriod: 7 * DAY,
+    lendPeriod: 1 * DURATION_UNIT,
     amount: 1,
     multiply: 1,
   },
   {
     id: "2",
-    lendPeriod: 30 * DAY,
+    lendPeriod: 2 * DURATION_UNIT,
     amount: 1,
     multiply: 1.05,
   },
   {
     id: "3",
-    lendPeriod: 60 * DAY,
+    lendPeriod: 3 * DURATION_UNIT,
     amount: 1,
     multiply: 1.1,
   },
   {
     id: "4",
-    lendPeriod: 90 * DAY,
+    lendPeriod: 4 * DURATION_UNIT,
     amount: 1,
     multiply: 1.15,
   },
   {
     id: "5",
-    lendPeriod: 120 * DAY,
+    lendPeriod: 5 * DURATION_UNIT,
     amount: 1,
     multiply: 1.2,
   },
